8c44dd6c47c45dca7eeec5a32712891d84271529
Kaylani Bochie <kaylani2@poli.ufrj.br>
2019-06-04 14:17:50 -0300

    Refactoring

--- scripts/computer_nodes/basicInstall.sh
+++ scripts/computer_nodes/basicInstall.sh
@@ -1,4 +1,13 @@
-## we will use mpstat to get the information and the mosquitto_pub software to transmit it
+#!/bin/bash
 
-apt-get install mpstat
-apt-get install mosquitto_clients
+### This script stablishes an ssh connection with each of the hosts in the network, clone the repository and install the crontab corresponding with the desired scripts.
+
+
+## TODO: script must clone the repo and install the new cron job
+SCRIPT="cd ~/; git clone https://github.com/kaylani2/labsensing; cd labsensing/scripts/computer_nodes"
+HOSTS=("ponte" "sepetiba")
+USERNAME="gta"
+
+for HOSTNAME in ${HOSTS} ; do
+    ssh -l ${USERNAME} ${HOSTNAME} "${SCRIPT}"
+done
