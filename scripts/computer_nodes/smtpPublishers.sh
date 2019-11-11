398790d0d0983c62dfc7ea166f2e0a015a257377
Kaylani Bochie <kaylani2@poli.ufrj.br>
2019-11-11 08:34:49 -0200

    Add features to smtpPublishers.sh

--- scripts/computer_nodes/smtpPublishers.sh
+++ scripts/computer_nodes/smtpPublishers.sh
@@ -10,23 +10,45 @@ server="gta.ufrj.br"
 port="25"
 
 
-########### Get information
-free -h | grep Mem: > mem.txt
-mpstat | grep all > cpu.txt ## may not be available in all machines
-## /\ it is not available on raspbian or mint
+############################## Get information ###############################
+
+############################## Memory information ###############################
+free -h > mem.txt
+#free -h | grep Mem: > mem.txt ## grep will remove the headers
+
+
+
+############################## CPU information ###############################
+#mpstat | grep all > cpu.txt ## may not be available in all machines
+## /\ it is not available on raspbian or mint, it's also not very descriptive
+#lscpu > cpu_info_lscpu.txt ## may not be available in all machines
+## cat /proc/cpuinfo > ... provides a more complete log
 cat /proc/cpuinfo > cpu_info.txt
-lshw  > machine_info.txt ## this gives most of the info, parsing it should almost be enough
-lscpu > cpu_info_lscpu.txt ## may not be available in all machines
 ## /\ it is not available on mint
+
+############################## General information ###############################
+lshw  > machine_info.txt ## this gives most of the info, parsing it should almost be enough
+
+############################## PCI information ###############################
 lspci > pci.txt
 ## /\ it is not available on raspbian
+
+############################## Processes information ###############################
 top -b -n 1 > top.txt
+
+############################## BIOS information ###############################
 ## \/ Get bios information
 #sudo dmidecode -t bios
+
+############################## Temperature information ###############################
 paste <(cat /sys/class/thermal/thermal_zone*/type) <(cat /sys/class/thermal/thermal_zone*/temp) | column -s $'\t' -t | sed 's/\(.\)..$/.\1C/' > temp.txt
 
+############################## Disk information ###############################
+## \/ Get disk usage
+df > disk.txt
 
-########## Process information
+
+########### Preprocess information before sending TODO
 #sed -i ':a;N;$!ba;s/\n/ /g' mem.txt # remove line feed
 #sed -i 's/ \{1,\}/;/g' mem.txt
 #sed -i ':a;N;$!ba;s/\n/ /g' cpu.txt # remove line feed
@@ -37,30 +59,30 @@ paste <(cat /sys/class/thermal/thermal_zone*/type) <(cat /sys/class/thermal/ther
 
 
 ########## Send information
-myHostname=$(echo $HOSTNAME)
-myHostname=(${myHostname//;/ })
-freeMemory=$(cat mem.txt)
-cpuUsage=$(cat cpu.txt)
-temperature=$(cat temp.txt)
-
-# create message
-function mail_input {
-echo "ehlo $(hostname -f)"
-echo "MAIL FROM: <$from>"
-echo "RCPT TO: <$to>"
-echo "DATA"
-echo "From: <$from>"
-echo "To: <$to>"
-echo "Subject: Testing SMTP Mail"
-echo "This is only a test. Please do not panic. If this works, then all is well, else all is not well."
-echo "<$freeMemory>"
-echo "<$cpuUsage>"
-echo "<$temperature>"
-echo "<$myHostname>"
-echo "."
-echo "quit"
-}
-mail_input | netcat $server $port || err_exit
+#myHostname=$(echo $HOSTNAME)
+#myHostname=(${myHostname//;/ })
+#freeMemory=$(cat mem.txt)
+#cpuUsage=$(cat cpu.txt)
+#temperature=$(cat temp.txt)
+#
+## create message
+#function mail_input {
+#echo "ehlo $(hostname -f)"
+#echo "MAIL FROM: <$from>"
+#echo "RCPT TO: <$to>"
+#echo "DATA"
+#echo "From: <$from>"
+#echo "To: <$to>"
+#echo "Subject: Testing SMTP Mail"
+#echo "This is only a test. Please do not panic. If this works, then all is well, else all is not well."
+#echo "<$freeMemory>"
+#echo "<$cpuUsage>"
+#echo "<$temperature>"
+#echo "<$myHostname>"
+#echo "."
+#echo "quit"
+#}
+#mail_input | netcat $server $port || err_exit
 
 ## CRONTAB:
 ## * * * * *  bash /home/kaylani/thisFile.sh
